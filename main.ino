5414d45710ceffee689e1db34826064e563d1b10
GUILHERME GONÇALVES GARCIA <082210014@faculdade.cefsa.edu.br>
2023-08-31 20:54:23 -0300

    Quarta-versão

--- main.ino
+++ main.ino
@@ -6,26 +6,41 @@
 
 const int pinoDHT11 = A1;
 
-
 RTC_DS3231 rtc; //OBJETO DO TIPO RTC_DS3231
 dht DHT;
-
 
 LiquidCrystal lcd(12, 11, 10, 5, 4, 3, 2); 
 
 int contador = 0;
 int luminosidadeTotal = 0;
-int umidadeTotal = 0;
-int temperaturaTotal = 0;
+float umidadeTotal = 0;
+float temperaturaTotal = 0;
 int luminosidadeMedia = 0;
-int umidadeMedia = 0;
-int temperaturaMedia = 0;
-int enderecoBase = EEPROM.read(0);
+float umidadeMedia = 0;
+float temperaturaMedia = 0;
 DateTime now;
 
 void setup() {
-  byte outputPin[] = {};
-  byte inputPin[] = {};
+    pinMode(13, OUTPUT);
+  
+  /***
+    Iterate through each byte of the EEPROM storage.
+
+    Larger AVR processors have larger EEPROM sizes, E.g:
+    - Arduno Duemilanove: 512b EEPROM storage.
+    - Arduino Uno:        1kb EEPROM storage.
+    - Arduino Mega:       4kb EEPROM storage.
+
+    Rather than hard-coding the length, you should use the pre-provided length function.
+    This will make your code portable to all AVR processors.
+  ***/
+
+  for (int i = 0 ; i < EEPROM.length() ; i++) {
+    EEPROM.write(i, 0);
+  }
+
+  // turn the LED on when we're done
+  digitalWrite(13, HIGH);
   Serial.begin(9600); //COMUNICAÇÃO ARDUINO PROMPT
     if(! rtc.begin()) { // SE O RTC NÃO FOR INICIALIZADO, FAZ
     Serial.println("DS3231 não encontrado"); //IMPRIME O TEXTO NO MONITOR SERIAL
@@ -47,15 +62,14 @@ void setup() {
 }
 
 void loop() {
-  
+  DHT.read11(pinoDHT11);
   calculaLuminosidade(); //VALOR DA LUMINOSIDADE EM %
   calculaUmidade(); //VALOR DA UMIDADE
   calculaTemperatura();
-  delay(10000);
+  delay(1000);
   contador++;
-  if(contador == 2
-  ){
-    DHT.read11(pinoDHT11); //LÊ AS INFORMAÇÕES DO SENSOR
+  if(contador == 2)//ALTERAR DEPOIS DE TESTES
+  {
     lcd.clear();
     now = rtc.now();
     luminosidadeMedia = calcularMedia(luminosidadeTotal);
@@ -64,10 +78,11 @@ void loop() {
     luminosidadeTotal = 0;
     umidadeTotal = 0;
     temperaturaTotal = 0;
+    contador = 0;
     lcd.setCursor(0,0);
-    lcd.print(String(now.day()) + "/" + String(now.month()) + "/" + String(now.year()));
+    lcd.print(String(now.day()) + "/" + String(now.month()) + "/" + String(now.year()) + " " + String(now.hour()) + ":" + String(now.minute()));
     lcd.setCursor(0,1);
-    lcd.print("L:" + String(luminosidadeMedia) + "%" + " U:" + String(umidadeMedia) + "% " + "T:" + String(temperaturaMedia)) + "°C";
+    lcd.print("L:" + String(luminosidadeMedia) + "%" + "U:" + String(umidadeMedia, 0) + "%" + "T:" + String(temperaturaMedia,0) + "C");
     conferir(luminosidadeMedia, umidadeMedia, temperaturaMedia);
     Serial.println("L:" + String(luminosidadeMedia));
     Serial.println(" U:" + String(umidadeMedia));
@@ -92,51 +107,56 @@ void calculaTemperatura(){
   temperaturaTotal = temperaturaTotal + temperature;
 }
 
-int calcularMedia(int valor){
-  return valor/6;
+float calcularMedia(int valor){
+  return valor/2;
 }
 
-void conferir(int luminosidade, int umidade, int temperatura){
-  contador = 0;
+void conferir(int luminosidade, float umidade, float temperatura){
   luminosidadeTotal = 0;
   temperaturaTotal = 0;
   umidadeTotal = 0;
-  String caracteres;
-  if(0< luminosidade< 30){
-    caracteres = "L" + luminosidade;
+  String caracteres = "";
+  if(luminosidade>30){
+      Serial.println("Conferir l");
+    caracteres = caracteres + "L" + String(luminosidade);
   }
-  if(30< umidade< 50){
-    caracteres += "U" + umidade;
+  if(umidade<30 || umidade>50){
+      Serial.println("Conferir u");
+    caracteres = caracteres + "U" + String(umidade, 0);
   }
-  if(15< temperatura< 25){
-    caracteres += "T" + temperatura;
+  if(temperatura<15 || temperatura > 25){
+      Serial.println("Conferir t");
+    caracteres = caracteres + "T" + String(temperatura, 0);
   }
   if(caracteres.length() > 0){
+    
+      Serial.println("Conferir alerta");
     alerta(caracteres);
   }
 }
 
 void alerta(String caracteres){
   now = rtc.now();
-  String mensagem = String(now.day()) + String(now.month()) + String(now.year()) + caracteres;
+  String mensagem = "";
+  mensagem = String(now.day()) + String(now.month()) + String(now.year()) + String(now.hour()) + String(now.minute()) + caracteres;
+  Serial.print(mensagem);
+  Serial.println();
   guardaEeprom(mensagem);
   ascendeLed();
   alertaSonoro();
 }
 
 void guardaEeprom(String mensagem){
-  if (mensagem.length()>EEPROM.length() || (enderecoBase+mensagem.length()) >EEPROM.length() ){
-    Serial.println ("A sua String não cabe na EEPROM");
-  }
-  else{
-    for (int i = 0 + 1; i<mensagem.length(); i++){
-       EEPROM.write(enderecoBase,mensagem[i]);
-       enderecoBase++;
+    for (int i = 0; i<mensagem.length(); i++){
+       EEPROM.write(0,mensagem[i]);
+       Serial.println(mensagem[i]);
     }
-    EEPROM.write(enderecoBase,'_');
-    EEPROM.write(0,enderecoBase);
-  }
+    for (int i = 0; mensagem.length(); i++){
+    
+       Serial.print(char(EEPROM.read(i)));
+    }
 }
+
 
 void ascendeLed(){
 
